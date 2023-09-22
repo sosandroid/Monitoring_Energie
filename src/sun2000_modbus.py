2755430ea3b8aa53ce6caa9c4d999451afd4ff53
sosandroid <ehavet@yahoo.fr>
2023-09-22 16:17:46 +0200

    Update sun2000_modbus.py
    Power calculation enhancement

--- src/sun2000_modbus.py
+++ src/sun2000_modbus.py
@@ -51,15 +51,26 @@ def get_sun2000_modbus(debug = False):
             #source_address=("192.168.0.57", 0),
         )
     client.connect()
-    sleep(0.5) # Connexion stabilization
-    power = client.read_holding_registers(32080, 2, unit) # active power, length 2, factor 1
+    sleep(1) # Connexion stabilization
+    # power = client.read_holding_registers(32080, 2, unit) # active power, length 2, factor 1
+    power = client.read_holding_registers(32069, 9, unit) # voltage and current on all 3 phases
     sleep(0.5) # Connexion stabilization
     temp = client.read_holding_registers(32087, 1, unit) #internal temperature, length 1, factor 10
     # sleep(0.5)
     # dailyEnergy = client.read_holding_registers(32114, 2, unit)  # Energie du jour facteur 100
     client.close()
+
+    # power calculation
+    v1 = power.registers[0] /10
+    v2 = power.registers[1] /10
+    v3 = power.registers[2] /10
+    i1 = (power.registers[3]*65535+power.registers[4])/1000
+    i2 = (power.registers[5]*65535+power.registers[6])/1000
+    i3 = (power.registers[7]*65535+power.registers[8])/1000
     
-    p = power.registers[0]*65535+power.registers[1]
+    p = v1*i1+v2*i2+v3*i3
+    
+    #p = power.registers[0]*65535+power.registers[1]
     t = temp.registers[0]/10
     
     return dict(PUI_PROD=p, TEMP_INT=t)
