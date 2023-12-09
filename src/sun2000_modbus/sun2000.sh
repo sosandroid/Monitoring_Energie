b08fa2cfe74048c44947d34dce840dc04e0f4a00
sosandroid <ehavet@yahoo.fr>
2023-12-09 18:32:14 +0100

    Create sun2000.sh
    Usefull for cron job tasking

---
+++ src/sun2000_modbus/sun2000.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+/usr/bin/python3 /home/emoncms/onduleur/sun2000_modbus.py &
+sleep 15 && /usr/bin/python3 /home/emoncms/onduleur/sun2000_modbus.py &
+sleep 30 && /usr/bin/python3 /home/emoncms/onduleur/sun2000_modbus.py &
+sleep 45 && /usr/bin/python3 /home/emoncms/onduleur/sun2000_modbus.py &
